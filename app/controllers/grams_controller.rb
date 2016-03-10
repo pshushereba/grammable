d71930b6a4a42b828def53ded0b64378c79d7f82
Patrick Shushereba <pshushereba@gmail.com>
2016-03-10 02:37:30 +0000

    Finished Lesson 14.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,7 +5,10 @@ class GramsController < ApplicationController
 	end
 
 	def destroy
-
+		@gram = Gram.find_by_id(params[:id])
+		return render_not_found if @gram.blank?
+		@gram.destroy
+		redirect_to root_path
 	end
 
 	def update
